07893ff697a3559b3071734a34543d34b96f2feb
refaktorco <refaktor.co@gmail.com>
2014-10-28 13:51:08 -0700

    -comment/pagination styles.

--- template.php
+++ template.php
@@ -145,6 +145,31 @@ function oyster_form_contact_site_form_alter(&$form, &$form_state, $form_id) {
   		
 }
 
+/**
+* Implements hook_form_comment_form_alter().
+*/
+function oyster_form_comment_form_alter(&$form, &$form_state) {
+
+   /* Remove field title and add placeholder for "Author" field */
+	$form['author']['name']['#attributes']['placeholder'] = t( 'Name' );
+  $form['author']['name']['#title'] = FALSE;
+	
+  /* Remove the "your name" elements for authenticated users */
+  if ($form['is_anonymous']['#value'] == false) {
+    $form['author']['#access'] = FALSE; 
+  }
+  
+  /* Remove field title and add placeholder for "Subject" field */
+	$form['subject']['#attributes']['placeholder'] = t( 'Subject' );
+	$form['subject']['#title'] = FALSE;
+	$form['subject']['#required'] = FALSE;
+	
+	/* Remove field title and add placeholder for "Comment Body" field */
+	$form['comment_body'][LANGUAGE_NONE][0]['#attributes']['placeholder'] = t( 'Comment' );
+	$form['comment_body']['und'][0]['#title'] = FALSE;
+	
+}
+
 
 /**
  * Add list classes for links in "Header Menu" region.
@@ -242,6 +267,71 @@ function oyster_block_view_alter(&$data, $block) {
     endforeach;
   }
 }
+
+/**
+ * Overrides theme_item_list().
+ */
+function oyster_item_list($variables) {
+  $items = $variables['items'];
+  $title = $variables['title'];
+  $type = $variables['type'];
+  $variables['attributes']['class'] = 'pagination pagerblock';
+  $attributes = $variables['attributes'];
+
+  // Only output the list container and title, if there are any list items.
+  // Check to see whether the block title exists before adding a header.
+  // Empty headers are not semantic and present accessibility challenges.
+  $output = '';
+  if (isset($title) && $title !== '') {
+    $output .= '<h3>' . $title . '</h3>';
+  }
+
+  if (!empty($items)) {
+    $output .= "<$type" . drupal_attributes($attributes) . '>';
+    $num_items = count($items);
+    $i = 0;
+    foreach ($items as $item) {
+      $attributes = array();
+      $children = array();
+      $data = '';
+      $i++;
+      
+      //if ( is_array($item) && in_array('pager-current', $item['class'])) {
+      if ( isset($item['class']) && is_array($item) && in_array('pager-current', $item['class'])) {
+        $item['class'] = array('active');
+        $item['data'] = '<a href="#">' . $item['data'] . '</a>';
+      }
+
+      if (is_array($item)) {
+        foreach ($item as $key => $value) {
+          if ($key == 'data') {
+            $data = $value;
+          }
+          elseif ($key == 'children') {
+            $children = $value;
+          }
+          else {
+            $attributes[$key] = $value;
+          }
+        }
+      }
+      else {
+        $data = $item;
+      }
+      if (count($children) > 0) {
+        // Render nested list.
+        $data .= theme_item_list(array('items' => $children, 'title' => NULL, 'type' => $type, 'attributes' => $attributes));
+      }
+      
+      $output .= '<li' . drupal_attributes($attributes) . '>' . $data . "</li>\n";
+    }
+    $output .= "</$type>";
+  }
+  
+  return $output;
+
+}
+
 
 /**
  * Modify theme_field()
