600744106d3f02c9002f4b2d1160c736c3cb9ebd
refaktorco <refaktor.co@gmail.com>
2014-10-31 10:04:26 -0700

    -Stuff

--- js/portfolio/portfolio_ribbon.js
+++ js/portfolio/portfolio_ribbon.js
@@ -1,20 +1,3 @@
-"use strict";
-	
-	var header = jQuery('.main_header'),
-    html = jQuery('html'),
-    body = jQuery('body'),
-    footer = jQuery('footer'),
-    window_h = jQuery(window).height(),
-    window_w = jQuery(window).width(),
-    main_wrapper = jQuery('.main_wrapper'),
-    site_wrapper = jQuery('.site_wrapper'),
-    setTop = 0,
-    fullscreen_block = jQuery('.fullscreen_block'),
-    is_masonry = jQuery('.is_masonry'),
-    grid_portfolio_item = jQuery('.grid-portfolio-item'),
-    pp_block = jQuery('.pp_block'),
-    head_border = 1;
-
 jQuery(document).ready(function($){
 	"use strict";
 	jQuery('html').addClass('fullscreen_page sticky_menu');
@@ -68,9 +51,9 @@ jQuery(window).load(function($){
 
 function ribbon_setup() {
 	"use strict";
-	var setHeight = window_h - header.height() - 20;
-	var setHeight2 = window_h - header.height() - jQuery('.slider_info').height() - 20;
-	jQuery('.fs_grid_gallery').height(window_h - header.height()-1);
+	var setHeight = jQuery(window).height() - jQuery('.main_header').height() - 20;
+	var setHeight2 = jQuery(window).height() - jQuery('.main_header').height() - jQuery('.slider_info').height() - 20;
+	jQuery('.fs_grid_gallery').height(jQuery(window).height() - jQuery('.main_header').height()-1);
 	jQuery('.currentStep').removeClass('currentStep');
 	jQuery('.slide1').addClass('currentStep');
 	jQuery('.num_current').text('1');
@@ -84,11 +67,11 @@ function ribbon_setup() {
 		jQuery(this).attr('data-offset',jQuery(this).offset().left);
 		jQuery(this).width(jQuery(this).find('img').width()+parseInt(jQuery(this).find('.slide_wrapper').css('margin-left')));
 	});
-	var max_step = -1*(jQuery('.ribbon_list').width()-window_w);
+	var max_step = -1*(jQuery('.ribbon_list').width()-jQuery(window).width());
 }
 function prev_slide() {
 	"use strict";
-	var max_step = -1*(jQuery('.ribbon_list').width()-window_w);
+	var max_step = -1*(jQuery('.ribbon_list').width()-jQuery(window).width());
 	var current_slide = parseInt(jQuery('.currentStep').attr('data-count'));
 	current_slide--;
 	if (current_slide < 1) {
@@ -105,7 +88,7 @@ function prev_slide() {
 }
 function next_slide() {
 	"use strict";
-	var max_step = -1*(jQuery('.ribbon_list').width()-window_w);
+	var max_step = -1*(jQuery('.ribbon_list').width()-jQuery(window).width());
 	var current_slide = parseInt(jQuery('.currentStep').attr('data-count'));
 	current_slide++;
 	if (current_slide > jQuery('.ribbon_list').find('li').size()) {
